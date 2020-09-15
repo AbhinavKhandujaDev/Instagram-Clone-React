2a6efcb14c36cb1f71bf47b61f85ceb16d80c0f2
abhikhanduja <abhikhanduja@gmail.com>
2020-09-15 21:05:07 +0530

    Post Component modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useState, useEffect } from 'react';
 import LoginSignupView from './components/login-signup/LoginSignupView';
-import Navbar from './components/Home/Navbar/Navbar';
+import HomePage from './components/Home/HomePage/HomePage.jsx';
+import { firebase } from './firebase';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
+
+  const [component, setComponent] = useState();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        setComponent(<HomePage />)
+      } else {
+        setComponent(<LoginSignupView />)
+      }
+    });
+  }, []);
   return (
     <div className="App flex-center">
-      <LoginSignupView />
-      {/* <Navbar/> */}
+      {component}
     </div>
   );
 }
